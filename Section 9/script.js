8b155a59fdf17d28c8f71eb7de332885260b36df
An ex-parrot <ronan.brattan@gmail.com>
2025-02-04 17:59:21 +1100

    destructuring arrays

--- Section 9/script.js
+++ Section 9/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  /*   order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    }, */
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,29 @@ const restaurant = {
     },
   },
 };
+/* 
+//destructuring
+const arr = [2, 3, 4]
+const [x, y, z] = arr; //x=2, y=3, z=4
+console.log(x, y, z);
+
+let [first, , second] = arr; // first = 2, second = 4;
+[first, second] = [second, first]; //first = 4, second = 2
+
+//receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//destructure inside destructure
+
+const nested = [2, 4, [5, 6,]];
+
+const [i, , [j, k]] = nested; //i = 2, j = 5, k = 6
+console.log(i, j, k);
+
+//default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r); //r = undefined
+const [l = 1, m = 1, n = 1] = [8, 9];
+console.log(l, m, n); //r = 1
+ */
